3d6d08842c78b58d7fde6fe0f66f37eb404199fc
guava2247749 <i27153153@gmail.com>
2024-11-15 11:20:13 +0800

    11/15

--- server.js
+++ server.js
@@ -6,6 +6,21 @@ var server = express();
 //web root
 server.use(express.static(__dirname+"/AgencyProject"));
 
+server.get("/services", (req,res)=>{
+    //DB find
+    var services=[
+        {icon:"fa-shopping-cart",heading:"E-Commerce",test:"Lorem ipsum dolor sit amet,consectetur adipisicing elit."},
+        {icon:"fa-laptop",heading:"Responsive Design",test:"Lorem ipsum dolor sit amet,consectetur adipisicing elit."}
+    ];
+    res.send(services);  
+})
+
+server.get("/profolio", (req,res)=>{
+    //DB
+
+    res.send("Profolio");  
+})
+
 server.listen(80, ()=>{
     console.log("Server is running at port 80.");
 })
