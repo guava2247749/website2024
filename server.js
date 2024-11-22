d90ece6551e031404c736240358545c9c6079395
guava2247749 <i27153153@gmail.com>
2024-11-22 10:51:25 +0800

    11/22

--- server.js
+++ server.js
@@ -6,23 +6,41 @@ var server = express();
 //web root
 server.use(express.static(__dirname+"/AgencyProject"));
 
-var DB =require("nedb-promises");
-var Profolio = DB.creat(__dirname+"/profolio.db");
+var DB = require("nedb-promises");
+var ProfolioDB = DB.create(__dirname+"/profolio.db");
 
-server.get("/services", (req,res)=>{
+ 
+// ProfolioDB.insert([
+//     { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
+//     { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
+//     { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" },
+//     { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
+//     { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
+//     { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" }
+// ])
+
+server.get("/services", (req, res)=>{
     //DB find
-    var services=[
-        {icon:"fa-shopping-cart",heading:"E-Commerce",test:"Lorem ipsum dolor sit amet,consectetur adipisicing elit."},
-        {icon:"fa-laptop",heading:"Responsive Design",test:"Lorem ipsum dolor sit amet,consectetur adipisicing elit."}
+    var Services=[
+        {icon: "fa-shopping-cart", heading:"E-Commerce", text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."},
+        {icon: "fa-laptop", heading:"Responsive Design", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."}
     ];
-    res.send(services);  
-})
+    res.send(Services);
+});
 
 server.get("/profolio", (req,res)=>{
-    //DB
+      //DB
+      ProfolioDB.find({}).then(results=>{
+        if(results != null){
+             res.send(results);
+        }else{
+            res.send("Error!");
+        }
+      })
+     
 
-    res.send("Profolio");  
 })
+
 
 server.listen(80, ()=>{
     console.log("Server is running at port 80.");
