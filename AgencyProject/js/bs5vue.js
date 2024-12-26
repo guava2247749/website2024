b73c3d841fbd69250ab193dc30581e231b876bf5
guava2247749 <i27153153@gmail.com>
2024-12-27 04:01:31 +0800

    12/27

--- AgencyProject/js/bs5vue.js
+++ AgencyProject/js/bs5vue.js
@@ -1,33 +1,28 @@
 const {createApp,ref}  = Vue;
 
-var myService = createApp({
+createApp({
     data() {
         return{
             Services:[
                 {icon: "fa-shopping-cart", heading:"E-Commerce", text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."},
-                {icon: "fa-laptop", heading:"Responsive Design", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."}
+                {icon: "fa-laptop", heading:"Responsive Design", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."},
+                {icon: "fa-lock", heading:"Web Security", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."}
             ]
         }
     }
 }).mount("#services");
 
-
-myService.Services.push({icon: "fa-lock", heading:"Web Security", text:"asdf adf adf"})
-
-var vueProfolio = createApp({
+createApp({
     data() {
         return{
-            Portfolio:[]
+            Portfolio:[
+                {portfolioModal:"#portfolioModal1",img:"img/portfolio/roundicons.png",name:"Round Icons",text:"Graphic Design"},
+                {portfolioModal:"#portfolioModal2",img:"img/portfolio/startup-framework.png",name:"Startup Framework",text:"Website Design"},
+                {portfolioModal:"#portfolioModal3",img:"img/portfolio/treehouse.png",name:"Treehouse",text:"Website Design"},
+                {portfolioModal:"#portfolioModal4",img:"img/portfolio/golden.png",name:"Golden",text:"Website Design"},
+                {portfolioModal:"#portfolioModal5",img:"img/portfolio/escape.png",name:"Escape",text:"Website Design"},
+                {portfolioModal:"#portfolioModal6",img:"img/portfolio/dreams.png",name:"Dreams",text:"YEAH"}
+            ]
         }
     }
 }).mount("#portfolio")
-
-$.ajax({
-    url:"/profolio",
-    method: "get",
-    dataType: "json",
-    success: results=>{
-        vueProfolio.Portfolio = results;
-    }
-
-})
